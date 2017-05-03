0bf77dc6d2f3acce49fdcbcf9f885aca73d854d2
Ricardo Aravena <raravena80@yahoo.com>
2017-05-03 20:24:43 +0000

    Add arraysplit solution

---
+++ arraysplit/README.md
@@ -0,0 +1,14 @@
+# arraysplit -- Split array where each piece is equal in sum
+
+Problem is given a list or array like this:
+
+list = [3, 4, 1, 2, 3, 5, 2]
+
+Output something like this:
+
+arr1: [3, 4, 1, 2] Sum = 10
+arr2: [3, 5, 2] Sum = 10
+
+Solutions are in Ruby:
+
+- arraysplit.rb - Solution in Ruby. Run with: ./arraysplit.rb
---
+++ arraysplit/arraysplit.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+list = [3, 4, 1, 2, 3, 5, 2]
+
+arr1 = []
+def get_sum_of_elements(arr)
+  sum = 0
+  arr.each do |e|
+    sum = sum + e
+  end
+  sum
+end
+
+list.each_with_index do |i, e|
+  arr1 = arr1.push(e)
+  arr2 = list[i+1..-1]
+  sum1 = get_sum_of_elements(arr1)
+  sum2 = get_sum_of_elements(arr2)
+  if sum1 == sum2
+    puts "Array1: #{arr1}, Sum: #{sum1}"
+    puts "Array2: #{arr2}, Sum: #{sum2}"    
+  end
+end
