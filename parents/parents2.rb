572cebc7dfd816c4f7bdbdb882869bbbb96d5c88
Ricardo Aravena <raravena@branch.io>
2017-10-04 17:35:22 -0700

    Problem info already on README.md

--- parents/parents2.rb
+++ parents/parents2.rb
@@ -1,44 +1,4 @@
 #!/usr/bin/env ruby
-#
-# 
-# Suppose we have some input data describing relationships between
-# parents and children over multiple generations. The data is formatted
-# as a list of (parent, child) pairs, where each individual is assigned
-# a unique integer identifier.
-# 
-# For example, in this diagram, 3 is a child of 1 and 2, and 5 is a child of 4:
-#             
-# 1   2   4
-#  \ /   / \
-#   3   5   8
-#    \ / \   \
-#     6   7   9
-# 
-# Write a function that takes this data as input and returns
-# two collections:
-# one containing all individuals with zero known parents,
-# and one containing all individuals with exactly one known parent.
-# 
-# Sample output:
-# Zero parents: 1, 2, 4
-# One parent: 5, 7, 8, 9
-# 
-# Feel free to solve this problem in any language of your choice.
-# 
-# parent_child_pairs = [[1, 3], [2, 3], [3, 6], [5, 6], [5, 7], [4, 5], [4, 8], [8, 9]]
-# 
-# 
-# 
-# def no_parents_oneparent(pairs)
-#    pairs.each do |i|
-#      first_elements
-#    end
-# 
-# 
-# end
-# 
-# 
-# 
 # 
 
 pairs = [[1, 3], [2, 3], [3, 6], [5, 6], [5, 7], [4, 5], [4, 8], [8, 9]]
